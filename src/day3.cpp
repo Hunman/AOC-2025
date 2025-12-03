59e0a71b52320822e99875e0b7c6eac53c76f043
Sándor Rátkai <sandor@ratk.ai>
2025-12-04 00:24:10 +0100

    Day 3 - multi-thread support
    (It makes things slower)

--- src/day3.cpp
+++ src/day3.cpp
@@ -1,11 +1,14 @@
 #include "day3/joltage_meter.hpp"
 #include "stopwatch.hpp"
 
+#include <execution>
 #include <filesystem>
 #include <fstream>
-#include <iostream>
+#include <ranges>
 
 namespace {
+    constexpr auto execution = std::execution::seq;
+
     std::vector<std::string> getLines() {
         std::ifstream in{std::filesystem::path(INPUT_DIRECTORY) / "day3.txt"};
 
@@ -26,23 +29,15 @@ namespace {
     }
 
     size_t exercise1(const std::vector<std::string> &lines) {
-        auto sum = 0uz;
-
-        for (const auto &line: lines) {
-            sum += JoltageMeter::maxDoubleDigit(line);
-        }
-
-        return sum;
+        return std::transform_reduce(execution, lines.begin(), lines.end(), 0L, std::plus{}, [](const auto &line) {
+            return JoltageMeter::maxDoubleDigit(line);
+        });
     }
 
     size_t exercise2(const std::vector<std::string> &lines) {
-        auto sum = 0uz;
-
-        for (const auto &line: lines) {
-            sum += JoltageMeter::maxTwelveDigit(line);
-        }
-
-        return sum;
+        return std::transform_reduce(execution, lines.begin(), lines.end(), 0, std::plus{}, [](const auto &line) {
+            return JoltageMeter::maxTwelveDigit(line);
+        });
     }
 } // namespace
 
